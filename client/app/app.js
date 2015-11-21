41993a2e65bbc8d405336e740a6c9a2eb7e88371
Nate Mow <natemow@gmail.com>
2015-11-20 19:10:00 -0500

    Built out initial song gen app.

--- client/app/app.js
+++ client/app/app.js
@@ -8,7 +8,8 @@
 
   var appConfig = {
     name: 'loremipsum',
-    apiRoot: 'http://' + document.location.hostname + '/api'
+    apiRoot: 'http://' + document.location.hostname + '/api',
+    lyricDelimiter: '|'
   };
 
   // Bind the module.
--- client/app/modules/songs/songs.js
+++ client/app/modules/songs/songs.js
@@ -6,15 +6,75 @@
 
   'use strict';
 
-  // Directive to handle input tasks.
-  var DirectiveInput = [
+  // Directive to handle output.
+  var DirectiveOutput = [
     function() {
-      return {
-        templateUrl: 'app/modules/songs/songs.html',
-        controller: ['$rootScope', '$scope', '$window', 'appConfig',
-          function($rootScope, $scope, $window, appConfig) {
+         return {
+        templateUrl: 'app/modules/songs/output.html',
+        controller: ['$scope', '$http', 'appConfig',
+          function($scope, $http, appConfig) {
 
-            // console.log(appConfig);
+            // Template variables.
+            $scope.artist = [];
+            $scope.songs = [];
+            $scope.song = [];
+
+            // Function to return only unique values from an array.
+            $scope.unique = function(value, index, self) {
+              return (self.indexOf(value) === index);
+            };
+
+            // Function to randomize an array.
+            $scope.shuffle = function(array) {
+              var currentIndex = array.length, temporaryValue, randomIndex;
+
+              // While there remain elements to shuffle...
+              while (0 !== currentIndex) {
+                // Pick a remaining element...
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex -= 1;
+
+                // And swap it with the current element.
+                temporaryValue = array[currentIndex];
+                array[currentIndex] = array[randomIndex];
+                array[randomIndex] = temporaryValue;
+              }
+
+              return array;
+            };
+
+            // Function to generate a random song.
+            $scope.generate = function() {
+
+              // Reset template variables.
+              $scope.artist = [];
+              $scope.songs = [];
+              $scope.song = [];
+
+              // Fetch the songs.
+              $http
+                .get(appConfig.apiRoot + '/songs', {})
+                .then(function(response) {
+                  if (response.status !== 200) {
+                    // TODO: handle errors.
+                    return false;
+                  }
+
+                  angular.forEach(response.data, function(song, ixSong) {
+                    $scope.artist.push(song.artist);
+                    $scope.songs.push(song.title);
+
+                    $scope.artist = $scope.artist.filter($scope.unique);
+
+                    var lyrics = song.lyrics.split(appConfig.lyricDelimiter);
+                    angular.forEach(lyrics, function(lyric, ixLyric) {
+                      $scope.song.push(lyric);
+                    });
+
+                    $scope.shuffle($scope.song);
+                  });
+                });
+            };
 
           }]
       };
@@ -24,6 +84,6 @@
   angular
     .module('songs', [
     ])
-    .directive('songs', DirectiveInput);
+    .directive('output', DirectiveOutput);
 
 })();
