6cc4ffc6e1066ae7ba1b520a062a077c39dade6d
Jeff Eoff <jeffeoff@gmail.com>
2016-07-18 11:48:58 -0400

    adding weird changes to vendor files, I guess?

--- server/vendor/bin/drush.php
+++ server/vendor/bin/drush.php
@@ -1,1 +1,12 @@
-../drush/drush/drush.php
+#!/usr/bin/env php
+<?php
+
+/**
+ * @file
+ * drush is a PHP script implementing a command line shell for Drupal.
+ *
+ * @requires PHP CLI 5.4.5, or newer.
+ */
+
+require __DIR__ . '/includes/preflight.inc';
+exit(drush_main());
